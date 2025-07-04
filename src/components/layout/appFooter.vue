153cbba799fe044023cc156bbf163d34ad62fdc6
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-07-04 21:56:30 +0330

    feat(component): implement layout components(header & footer)

---
+++ src/components/layout/appFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="bg-[#f1f1f1] p-4 flex justify-center border-t border-t-[#ccc]">
+        <h1>Footer</h1>
+    </footer>
+</template>
---
+++ src/components/layout/appHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header class="bg-[#f1f1f1] p-4 flex justify-center border-b border-b-[#ccc]">
+        <h1>Header</h1>
+    </header>
+</template>
